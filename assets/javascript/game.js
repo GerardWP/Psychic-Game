2d3e42f6b21776c05d7377fc8485a2612f6192c9
Gerard Pelly <gerardwp@gmail.com>
2019-07-01 22:48:01 +1000

    forgot to commit for ages sorry. this was hard. made variables clearer and added variable for guesses allowed. function for getting random letter from array. and funtion for onkeyup. seems to be working, have not linked to innerHTML yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,52 @@
 //  by Gerard Pelly
 //
 
-var winText = document.getElementById("wins"),
-    loseText = document.getElementById("losses"),
-    guessText = document.getElementById("guesses"),
-    guessLeftText = document.getElementById("guessesLeft");
+var winText = document.getElementById("wins");
+var loseText = document.getElementById("losses");
+var guessNum = document.getElementById("remaining");
+var myGuesses = document.getElementById("guesses");
 
-var psychicChoce = ["a", "b", "c", "d", "e", "f",
+var won = 0;
+var lost = 0;
+var guessed = [];
+
+var letters = ["a", "b", "c", "d", "e", "f",
     "g", "h", "i", "j", "k", "l", "m",
     "n", "o", "p", "q", "r", "s", "t",
     "u", "v", "w", "x", "y", "z"
 ];
+
+var guessesAllowed = 10;
+
+function getRandomLetter() {
+    return letters[Math.floor(Math.random() * letters.length)];
+}
+
+var compChoice = getRandomLetter();
+
+document.onkeyup = function (event) {
+
+    var keyPress = event.key;
+
+
+    if (keyPress == compChoice) {
+        ++won;
+        compChoice = getRandomLetter();
+        guessed = [];
+    } else {
+        guessed.push(keyPress);
+        var guessesLeft = guessesAllowed - guessed.length;
+        if (guessesLeft <= 0) {
+            ++lost;
+            guessed = [];
+            compChoice = getRandomLetter();
+        }
+    }
+
+    console.log("-------------------");
+    console.log("comp choice: " + compChoice);
+    console.log("won: " + won);
+    console.log("lost: " + lost);
+    console.log("guessed: " + guessed);
+    console.log("guessesLeft: " + (guessesAllowed - guessed.length));
+}
