dea8350fa425e814f626f09bbb04c1e81e4f1e09
Gerard Pelly <gerardwp@gmail.com>
2019-07-01 17:39:44 +1000

    set up html page, linked to css and js files, declared some variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//
+//  Psychic Game
+//  by Gerard Pelly
+//
+
+var winText = document.getElementById("wins"),
+    loseText = document.getElementById("losses"),
+    guessText = document.getElementById("guesses"),
+    guessLeftText = document.getElementById("guessesLeft");
+
+var psychicChoce = ["a", "b", "c", "d", "e", "f",
+    "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y", "z"
+];
